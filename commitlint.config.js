92614d677cab9b1365abb0049d41a19c28833714
Sanjay Rai <sanrai@adobe.com>
2024-11-19 03:57:53 -0800

    next attempt

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'scope-enum': [2, 'always', []], // disable default scope enum check
-        'scope-pattern': [2, 'always', /MWPW-\d+/], // enforce MWPW-numbers in scope
-    }
+        'scope-empty': [2, 'never'], // require scope
+        'scope-pattern': [2, 'always', /^MWPW-\d+$/], // enforce MWPW-numbers ONLY in scope
+        'header-pattern': [2, 'always', /^(?:feat|fix|docs|style|refactor|perf|test|chore|build|ci|revert)\(MWPW-\d+\):/], // stricter header pattern
+    },
 };
